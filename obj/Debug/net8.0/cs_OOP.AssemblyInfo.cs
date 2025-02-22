76385943590ad7e955c9565d57f6a08ccb878f14
Yutai Liu <yutailiu@Yutais-MBP.attlocal.net>
2025-02-22 16:57:30 -0500

    preparing for push

--- obj/Debug/net8.0/cs_OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0/cs_OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e311b99c5f09de491294bf47e96a0d974b8fbeab")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
